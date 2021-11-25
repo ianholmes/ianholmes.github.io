cfa25c4e75fdd2d705ab3fb4564829319611f8d3
Ian Holmes <ian.holmes@ed.ac.uk>
2021-11-25 14:39:03 +0000

    Second web map test

---
+++ data/spot_heights_4.js
@@ -0,0 +1,1 @@
+var json_spot_heights_4 = {"type":"FeatureCollection","name":"spot_heights_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","height":100.0},"geometry":{"type":"Point","coordinates":[0.0,0.0]}}]}
